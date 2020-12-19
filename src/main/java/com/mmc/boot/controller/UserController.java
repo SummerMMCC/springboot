5bb5b0a6844628091dbb343d9f042ff659b36e05
SummerMMC <liusongning@hotmail.com>
2020-12-19 13:58:53 +0800

    add

---
+++ src/main/java/com/mmc/boot/controller/UserController.java
@@ -0,0 +1,13 @@
+package com.mmc.boot.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping(value = "/user")
+public class UserController {
+    @RequestMapping(value = "/index")
+    public String index() {
+        return "index";
+    }
+}
